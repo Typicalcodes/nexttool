fc317e0546db001c963a3b5715ae2428e60b7fa3
Typicalcodes <100959820+Typicalcodes@users.noreply.github.com>
2024-03-29 10:15:53 +0530

    All things Done

--- app/QA/page.js
+++ app/QA/page.js
@@ -1,13 +1,20 @@
 "use client"
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { MyContext } from '../Provider'
 
 export default function Qa() {
     const {Dataqa}= useContext(MyContext);
+    const [qaData, setqaData] = useState([])
     useEffect(() => {
-        
+        const jsonObject = JSON.parse(Dataqa);
+        setqaData(jsonObject.data.questions_and_answers)
+console.log(qaData);
 
     }, [])
+    useEffect(() => {
+console.log(qaData)
+    }, [qaData])
+    
     
   return (
     <>
@@ -15,7 +22,12 @@ export default function Qa() {
       <div className="flex flex-col">
         <label type></label>
       
-
+        {qaData && qaData.map((item,index)=>{
+            return <div key={index}><div>
+                <h2>{item.question}</h2>
+                <h4>{item.answer}</h4>
+                </div></div>
+        })}
         <button
 
           className="px-4 py-2 bg-green-500 mt-4 text-white self-start rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600"
